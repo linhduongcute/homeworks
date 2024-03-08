5d2eddcf7432aaf4b6b27a97dd7b6124f5d10772
linhduongcute <160800570+linhduongcute@users.noreply.github.com>
2024-03-08 17:01:11 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -2,28 +2,21 @@
 
 using namespace std;
 
-void Secretnumber(int a) // ham nhap vao mot so
-bool checkcon(int secnum); //ham duyet dieu kien
-bool checkNum(int guessNumber, int SecretNumber)  // kiem tra ket qua
-int GuessNum(int SecretNum, int GuessNum); //ham duyet ket qua may doan
-
 int main() {
-    int secretNum=80;
-    // secretNum=Secretnumber;
-    // if (checkcon(secretNum)== false) cout << "please give me a number from 1 to 100: ";
-    int GuessNumber;
+    int low=1, high=100;
+    char con;
+    do{
+        int guessnum=(low+high)/2;
+        cout << guessnum << endl;
+        cout << "Is that right?" << endl;
+        cin >> condition;
+        if (condition=='>') {
+        high = guessnum;
+        }
+        else {
+        low = guessnum;
+        }
+    }while (condition!='=');
+    cout << "congratulatuion!";
     return 0;
-}
-void Secretnumber(int a) {
-    int SN;
-    cout << "please give me a number from 1 to 100:";
-    cin >> SN;
-}
-
-int guessNum(int Guess, int secret, int Low, int High) {
-    if (Guess == secret) return guess;
-    else if (Guess > secret) {
-        Guess = high;
-        low
-    }
 }
