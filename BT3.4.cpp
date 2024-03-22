e9d0d754ddda12ad858fb36ad564783907cc7e3d
linhduongcute <160800570+linhduongcute@users.noreply.github.com>
2024-03-22 21:03:24 +0700

    Create BT3.4.cpp

---
+++ BT3.4.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    int sum=1;
+    int len= s.length();
+    for (int i=1; i<len; i++) {
+        if (s[i]>='A'&& s[i]<='Z') sum ++;
+    }
+    return sum;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
